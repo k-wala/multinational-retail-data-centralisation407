6d8203b8b93ce32e6c6a41f9490f4045488adbbd
k-wala <kkuntawala@gmail.com>
2024-01-08 17:20:52 +0000

    Adding method to read db credentials

--- data_extraction.py
+++ data_extraction.py
@@ -1,1 +1,9 @@
+import yaml
+
 class DataExtractor:
+
+    def read_db_creds(self):
+        with open ('db_creds.yaml') as creds_file:
+            db_creds_dict = yaml.safe_load(creds_file)
+            return db_creds_dict
+
